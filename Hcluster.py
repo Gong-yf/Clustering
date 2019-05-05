65c79b95885c54779ef27732f1cbd94f9dfae5ec
gongyf <gong.yf@outlook.com>
2019-05-05 17:50:15 +0800

    add the test case

--- Hcluster.py
+++ Hcluster.py
@@ -61,6 +61,7 @@ class Hcluster(object):
             min_dist = math.inf
             nodes_len = len(nodes)
             closest_part = None
+            #循环寻找距离最短的类
             for i in range(nodes_len - 1):
                 for j in range(i + 1, nodes_len):
                     d_key = (nodes[i].iden, nodes[j].iden)
@@ -70,7 +71,7 @@ class Hcluster(object):
                     if d < min_dist:
                         min_dist = d
                         closest_part = (i, j)
-            # 合并两个聚类
+            #合并聚类
             part1, part2 = closest_part
             node1, node2 = nodes[part1], nodes[part2]
             new_center = self.center(node1,node2)
@@ -82,7 +83,7 @@ class Hcluster(object):
                                    count=node1.count + node2.count,
                                    height = max(node1.height,node2.height)+1)
             currentclustid -= 1
-            del nodes[part2], nodes[part1]   # 一定要先删索引大的,先删小的会改变后面的索引
+            del nodes[part2], nodes[part1]
             nodes.append(new_node)
         self.nodes = nodes
         self.calc_label()
@@ -114,15 +115,32 @@ if __name__ == '__main__':
     
     t1 = datetime.datetime.now()
     
-    X1 = [random.random()*10+1 for i in range(100)]
-    Y1 = [random.uniform(0,2)*val+random.uniform(5,20) for val in X1]
-    X2 = [random.random()*10-1 for i in range(100)]
-    Y2 = [random.uniform(-2,0)*val+random.uniform(-20,5) for val in X2]
+    n = 50
+    n = n//2
+    X = [random.random()*10 for i in range(n)]
+    Y1 = [random.uniform(0,2)*val+random.uniform(5,30) for val in X]+[random.uniform(-2,0)*val+random.uniform(-20,5) for val in X]
+    Y2 = [random.uniform(-2,0)*val+random.uniform(-30,5) for val in X]+[random.uniform(0,2)*val+random.uniform(5,20) for val in X]
+    X1 = [random.random()*10 for i in range(n)]+[random.random()*10+10 for i in range(n)]
+    X2 = [random.random()*10 for i in range(n)]+[random.random()*10-10 for i in range(n)]
     X = X1+X2
     Y = Y1+Y2
+    data = np.array([(x,y) for x,y in zip(X,Y)])
     
     fig1 = plt.figure(figsize = (2,6),dpi = 200)
-    ax0 = fig1.add_subplot(311)
-    ax0.scatter(X,Y,edgecolor='none',s = 5)
+    ax1 = fig1.add_subplot(311)
+#    ax0.scatter(X,Y,edgecolor='none',s = 5)
+    ax1.scatter(X1,Y1,edgecolor='none',s = 5)
+    ax1.scatter(X2,Y2,edgecolor='none',s = 5)
     
-    H = He
+    k = 4
+    H = Hcluster(k=k)
+    H.fit(data)
+    datafig = [[] for i in range(k)]
+    for i,d in enumerate(data):
+        datafig[H.labels[i]].append(d)
+       
+    ax2 = fig1.add_subplot(312)
+    for i in range(k):
+        d = np.array(datafig[i])
+        ax2.scatter(d[:,0],d[:,1],edgecolor='none',s = 5)
+        
