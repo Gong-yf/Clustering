8f728f95b991ebfce0843c2ff21e8510886e7eb7
gongyf <gong.yf@outlook.com>
2019-05-31 10:05:34 +0800

    MaxHeap.py finished

--- MaxHeap.py
+++ MaxHeap.py
@@ -1,8 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+@author: yifan.gong
+"""
 class MaxHeap(object):
-    '''优先队列'''
-    def __init__(self, max_size, data=None):
-        self.data = data
+    '''最大堆'''
+    def __init__(self, max_size=50, data=None):
         self.max_size = max_size
+        self.data = data
         self.result_list = [None]*max_size
         self.size = 0
 
@@ -11,7 +15,7 @@ class MaxHeap(object):
     def is_full(self):
         return self.size == self.max_size
 
-    '''加入一个新值'''
+    '''加入一个新值,如果队列已满，则替换掉最大的元素'''
     def add(self, value):
         if self.is_full:
             if value < self.result_list[0]:
@@ -32,7 +36,7 @@ class MaxHeap(object):
         self.shift_down(0)
         return r
 
-    def shift_down(self, idx):
+    def shift_up(self, idx):
         assert idx < self.size, 'index out of range'
         parent = (idx-1)//2
         while parent >= 0 and self.result_list[parent] < self.result_list[idx]:
@@ -40,5 +44,34 @@ class MaxHeap(object):
             idx = parent
             parent = (idx-1)//2
 
-    def shift_up(self, x):
-        pass
+    def shift_down(self, idx):
+        child = 2*(idx+1)-1
+        while child < self.max_size and self.result_list[child]:
+            if child+1 < self.max_size and self.result_list[child+1] and self.result_list[child+1] > self.result_list[child]:
+                child += 1
+            if self.result_list[idx] < self.result_list[child]:
+                self.result_list[child], self.result_list[idx] = self.result_list[idx], self.result_list[child]
+                idx = child
+            else:
+                break
+
+    '''生成最大堆：将data中元素依次加入'''
+    def fit(self):
+        for num in self.data:
+            self.add(num)
+
+    '''堆排序：将堆顶元素依次推出'''
+    def heapsort(self):
+        r = []
+        self.fit()
+        while self.size > 0:
+            r.append(self.pop())
+        return r
+
+
+if __name__ == '__main__':
+    import random
+
+    data = [random.randint(1, 100) for i in range(30)]
+    mh = MaxHeap(50, data)
+    sorted_list = mh.heapsort()
